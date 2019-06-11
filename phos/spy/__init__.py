a42bd796284deb54094221fbf7d1d68ef8805a1d
James Knighton <iamknighton@gmail.com>
2019-06-11 11:13:29 -0700

    RowPerEpoch.

---
+++ phos/spy/__init__.py
@@ -0,0 +1,1 @@
+from .row_per_epoch import *
---
+++ phos/spy/row_per_epoch.py
@@ -0,0 +1,74 @@
+from .reporter import Reporter
+from .util.batch_result_buffer import BatchResultBuffer
+
+
+mean = lambda xx: sum(xx) / len(xx)
+
+
+class RowPerEpoch(Reporter):
+    """
+    Writes a results row after each epoch, forming a happy little ASCII table.
+
+    Format:
+        * epoch
+        * loss (train, val, difference)
+        * accuracy % (train, val, difference)
+        * time ms (train forward, train backward, val forward)
+    """
+
+    def __init__(self, filename=None):
+        if filename is None:
+            filename = '/dev/stdout'
+        self.filename = filename
+
+    def on_epoch_begin(self, epoch, model):
+        """
+        Handle epoch begin.
+        """
+        self.buffer = BatchResultBuffer()
+
+    def on_train_on_batch_end(self, epoch, batch, *args):
+        """
+        Handle the results of one training batch.
+        """
+        self.buffer.train.add(*args)
+
+    def on_validate_on_batch_end(self, epoch, batch, *args):
+        """
+        Handle the results of one validation batch.
+        """
+        self.buffer.val.add(*args)
+
+    def on_epoch_end(self, epoch, model):
+        """
+        Handle epoch end.
+        """
+        x = self.buffer.dump()
+
+        t = x['train']
+        t_loss = mean(t['loss'])
+        t_acc = mean(t['accuracy']) * 100
+        t_fwd = mean(t['forward'])
+        t_fwd = int(t_fwd * 1000)
+        t_bwd = mean(t['backward'])
+        t_bwd = int(t_bwd * 1000)
+
+        v = x['val']
+        v_loss = mean(v['loss'])
+        v_acc = mean(v['accuracy']) * 100
+        v_fwd = mean(v['forward'])
+        v_fwd = int(v_fwd * 1000)
+
+        d_loss = t_loss - v_loss
+        d_acc = t_acc - v_acc
+
+        epoch_part = '%6d' % epoch
+        loss_part = '%6.2f %6.2f : %6.2f' % (t_loss, v_loss, d_loss)
+        acc_part = '%6.2f%% %6.2f%% : %6.2f%%' % (t_acc, v_acc, d_acc)
+        time_part = '%4d %4d : %4d' % (t_fwd, t_bwd, v_fwd)
+        parts = epoch_part, loss_part, acc_part, time_part
+        line = ' | '.join(parts)
+        line = '| %s |\n' % line
+
+        with open(self.filename, 'w') as out:
+            out.write(line)

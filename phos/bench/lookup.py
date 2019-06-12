6586f2cb0610a35dc6b0a1371edadb27d8dafd8a
James Knighton <iamknighton@gmail.com>
2019-06-12 09:02:49 -0700

    lookup/view.

---
+++ phos/bench/lookup.py
@@ -0,0 +1,90 @@
+import itertools
+import json
+import numpy as np
+import os
+
+
+_RESOLUTIONS = 1, 10, 100
+_SPLITS = 'train', 'val'
+_ATTRIBUTES = 'loss', 'accuracy', 'forward', 'backward'
+
+
+_STATIC_QUERY_OPTIONS = {
+    'resolution': _RESOLUTIONS,
+    'split': _SPLITS,
+    'attribute': _ATTRIBUTES,
+}
+
+
+def get_settings(bench_dir):
+    f = '%s/settings.json' % bench_dir
+    return json.load(open(f))
+
+
+def get_done_models(bench_dir):
+    d = '%s/model/' % bench_dir
+    models = []
+    for model in sorted(os.listdir(d)):
+        f = '%s/model/%s/done.txt' % (bench_dir, model)
+        if os.path.exists(f):
+            models.append(model)
+    return models
+
+
+def get_static_query_options():
+    return _STATIC_QUERY_OPTIONS
+
+
+def get(k2v, k, vv):
+    x = k2v.get(k)
+    if x is None:
+        return vv
+    elif isinstance(x, list):
+        if x:
+            return x
+        else:
+            return vv
+    else:
+        return [x]
+
+
+def product(models, resolutions, splits, attributes):
+    for model, resolution, split, attribute in itertools.product(
+            models, resolutions, splits, attributes):
+        if split == 'val' and attribute == 'backward':
+            continue
+        yield model, resolution, split, attribute
+
+
+def query_results(bench_dir, x):
+    models_done = get_done_models(bench_dir)
+    models = get(x, 'model', models_done)
+    models = sorted(filter(lambda s: s in models_done, models))
+
+    resolutions = get(x, 'resolution', _RESOLUTIONS)
+    splits = get(x, 'split', _SPLITS)
+    attributes = get(x, 'attribute', _ATTRIBUTES)
+
+    pairs = []
+    for model, resolution, split, attribute in product(models, resolutions, splits,
+                                                       attributes):
+        k = {
+            'model': model,
+            'resolution': resolution,
+            'split': split,
+            'attribute': attribute,
+        }
+
+        f = '%s/model/%s/result/%d/%s_%s.npy' % \
+            (bench_dir, model, resolution, split, attribute)
+        x = np.fromfile(f, np.float32)
+        v = x.tolist()
+
+        pairs.append((k, v))
+
+    return pairs
+
+
+def get_blurb(bench_dir, model, epoch):
+    f = '%s/model/%s/blurb/%d.json' % (bench_dir, model, epoch)
+    return json.load(open(f))
--- phos/bench/view.py
+++ phos/bench/view.py
@@ -1,71 +1,65 @@
 #!/usr/bin/env python3
 
-import itertools
-import json
-import numpy as np
-import os
+from argparse import ArgumentParser
+from flask import Flask, jsonify, request, Response
+from mimetypes import guess_type
+
+from .lookup import get_blurb, get_done_models, get_settings, \
+    get_static_query_options, query_results
 
 
-RESOLUTIONS = 1, 10, 100
-SPLITS = 'train', 'val'
-ATTRIBUTES = 'loss', 'accuracy', 'forward', 'backward'
+def parse_flags():
+    a = ArgumentParser()
+    a.add_argument('--host', type=str, default='0.0.0.0')
+    a.add_argument('--port', type=int, default=1337)
+    a.add_argument('--dir', type=str, default='data/bench/example/')
+    a.add_argument('--www', type=str, default='phos/bench/www/')
+    return a.parse_args()
 
 
-def each_done_model(bench_dir):
-    d = '%s/model/' % bench_dir
-    for model in sorted(os.listdir(d)):
-        f = '%s/model/%s/done.txt' % (bench_dir, model)
-        if os.path.exists(f):
-            yield model
+flags = parse_flags()
+
+app = Flask(__name__)
 
 
-def get(k2v, k, vv):
-    x = k2v.get(k)
-    if x is None:
-        return vv
-    elif isinstance(x, list):
-        if x:
-            return x
-        else:
-            return vv
-    else:
-        return [x]
+@app.route('/api/get_settings', methods=['POST'])
+def serve_api_get_settings():
+    x = get_settings()
+    return jsonify(x)
 
 
-def product(models, resolutions, splits, attributes):
-    for model, resolution, split, attribute in itertools.product(
-            models, resolutions, splits, attributes):
-        if split == 'val' and attribute == 'backward':
-            continue
-        yield model, resolution, split, attribute
+@app.route('/api/get_done_models', methods=['POST'])
+def serve_api_get_done_models():
+    x = get_done_models(flags.dir)
+    return jsonify(x)
 
 
-def each_query_result(bench_dir, x):
-    done_models = list(each_done_model(bench_dir))
-    models = get(x, 'model', done_models)
-    models = sorted(filter(lambda s: s in done_models, models))
-
-    resolutions = get(x, 'resolution', RESOLUTIONS)
-    splits = get(x, 'split', SPLITS)
-    attributes = get(x, 'attribute', ATTRIBUTES)
-
-    for model, resolution, split, attribute in product(models, resolutions, splits,
-                                                       attributes):
-        k = {
-            'model': model,
-            'resolution': resolution,
-            'split': split,
-            'attribute': attribute,
-        }
-
-        f = '%s/model/%s/result/%d/%s_%s.npy' % \
-            (bench_dir, model, resolution, split, attribute)
-        x = np.fromfile(f, np.float32)
-        v = x.tolist()
-
-        yield k, v
+@app.route('/api/get_static_query_options', methods=['POST'])
+def serve_api_get_static_query_options():
+    x = get_static_query_options()
+    return jsonfiy(x)
 
 
-def get_blurb(bench_dir, model, epoch):
-    f = '%s/model/%s/blurb/%d.json' % (bench_dir, model, epoch)
-    return json.load(open(f))
+@app.route('/api/query_results', methods=['POST'])
+def serve_api_query_results():
+    x = query_results(flags.dir, request.json)
+    return jsonify(x)
+
+
+@app.route('/static/<basename>')
+def serve_static(basename):
+    assert '..' not in basename
+    f = '%s/%s' % (flags.www, basename)
+    text = open(f).read()
+    mimetype = guess_type(basename)[0]
+    return Response(text, mimetype=mimetype)
+
+
+@app.route('/')
+def serve():
+    f = '%s/index.html' % flags.www
+    return open(f).read()
+
+
+
+app.run(host=flags.host, port=flags.port)
